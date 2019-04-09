67c465a3fa336414b906bdcaa237620809fa7408
Fidalgo <pedrofidalgo7@gmail.com>
2019-04-09 11:56:08 +0100

    Initial setup of Layout Component

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from './Layout.module.css';
+
+const Layout = props => (
+  <React.Fragment>
+    <div>Toolbar, SideDrawer, Backdrop</div>
+    <main className={styles.content}>{props.children}</main>
+  </React.Fragment>
+);
+
+export default Layout;
---
+++ src/containers/BurguerBuilder/BurguerBuilder.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+
+const BurguerBuilder = props => {
+  return (
+    <React.Fragment>
+      <div>Burguer Built</div>
+      <div>Build Controlls</div>
+    </React.Fragment>
+  );
+};
+
+export default BurguerBuilder;
